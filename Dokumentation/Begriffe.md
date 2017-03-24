daa72a1a35dd519fe1c95b7f5702347b1dc33929
Ulf Lempart <ulf.steffen.lempart@comparex.de>
2017-03-24 17:01:33 +0100

    #1 - 1. Funktion

---
+++ Dokumentation/Begriffe.md
@@ -0,0 +1,8 @@
+Begriffserklärung anhand eines Beispiels:
+Prj-123456 - Dies ist ein Projekt
+Issue Kopfzeile: Prj-123456 - Dies ist ein Projekt
+Projekt: Prj
+Issue Nummer: 123456
+Issue ID : Prj-123456
+Issue Titel : Dies ist ein Projekt
+
---
+++ src/Gemini2Git.Test/T_Helper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Gemini2Git;
+
+
+namespace Gemini2Git.Test
+{
+    [TestClass]
+    public class T_Helper
+    {
+        /// <summary>
+        /// Extrahiere aus Kopfzeile in die ID und Titel
+        /// </summary>
+        [TestMethod]
+        public void Split_Kopfzeile_in_ID_und_Titel()
+        {
+            string[] expected = new string[2] { "Prj-123456", "Dies ist ein Projekt" };
+
+            string kopfzeile = "Prj-123456 - Dies ist ein Projekt";
+            string[] actual = Helper.Split_Kopfzeile_in_ID_und_Titel(kopfzeile);
+            Assert.AreEqual(expected[0], actual[0]);
+            Assert.AreEqual(expected[1], actual[1]);
+        }
+    }
+}
